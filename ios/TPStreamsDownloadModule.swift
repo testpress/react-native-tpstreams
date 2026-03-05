33d835ec941c468b0ffeb474bca7c1a0c254400b
syed-tp <syed@testpress.in>
2026-03-05 12:36:52 +0530

    fix: add startDownload in iOS

--- ios/TPStreamsDownloadModule.swift
+++ ios/TPStreamsDownloadModule.swift
@@ -51,7 +51,16 @@ class TPStreamsDownloadModule: RCTEventEmitter, TPStreamsDownloadDelegate {
     
     @objc
     func startDownload(_ videoId: String, accessToken: String, resolution: String?, metadata: NSDictionary?, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        resolve(nil)
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            
+            let metadataDict = metadata as? [String: Any]
+            let res = (resolution?.isEmpty ?? true) ? nil : resolution
+            let presentingVC = (res == nil) ? RCTPresentedViewController() : nil
+            
+            self.downloadManager.startDownload(assetID: videoId, accessToken: accessToken, resolution: res, allowResolutionFallback: true, metadata: metadataDict, presentingViewController: presentingVC, completion: nil)
+            resolve(nil)
+        }
     }
 
     @objc
